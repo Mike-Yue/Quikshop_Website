ce88f218092d40123e25b71189fd84479422de5f
Mike <mikeyue97@gmail.com>
2018-03-17 22:45:21 -0700

    nodejs server up and running

--- myfirst.js
+++ myfirst.js
@@ -2,8 +2,16 @@ var express = require('express');
 var app = express();
 var path = require("path");
 var mysql = require("mysql");
+var i;
+var blockNum = [];
+var nonce = [];
+var data = [];
+var prevHash = [];
+var currHash = [];
 
 app.use(express.static('Script'));
+app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
 
 var con = mysql.createConnection({
 	host: "localhost",
@@ -12,18 +20,29 @@ var con = mysql.createConnection({
 	database: "blocks"
 });
 
-con.connect(function(err){
-	if(err) throw err;
-	console.log("connected!");
+
+/*app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname + '/index.html'));
+}); */
+
+app.get('/', function (req, res) {
+	con.connect(function(err){
 
 	con.query("SELECT * FROM blockchain", function(err, result, fields){
 		if(err) throw err;
+		for(i = 0; i < result.length; i++){
+			blockNum[i] = result[i].number;
+			nonce[i] = result[i].nonce;
+			data[i] = result[i].data;
+			prevHash[i] = result[i].prev_hash;
+			currHash[i] = result[i].curr_hash;
+		}
 		console.log(result);
+		});
 	});
-});
-
-app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname + '/index.html'));
+    res.render("pugprac", {
+    	blockNum, nonce, data, prevHash, currHash
+    });
 });
 
 
